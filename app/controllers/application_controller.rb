85296e976cb056b3164b6b1f0ec00f29f68a8651
ken_yamamoto <ken.yamamoto@coincheck.com>
2017-10-14 20:37:54 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+   render html: "hello,mundo!"
+  end
+  def goodbye
+    render html: "goodbye mundo!"
+  end
+end
+
+
